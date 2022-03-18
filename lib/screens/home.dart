19d0bcf1285a3b1f0d2489fb0b2698a488a23480
Drallireeh <kenny.herillard@hotmail.fr>
2022-03-18 17:31:29 +0100

    Exercice 8 done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:exercices/components/exercices/exercice_7.dart';
-import 'package:exercices/components/drawer.dart';
+import 'package:exercices/components/list.dart';
+import 'package:exercices/components/carousel/index.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -15,7 +16,9 @@ class _MyHomePageState extends State<MyHomePage> {
   int index = 0;
 
   List<Widget> list = [
-    const Exercice7(),
+    const CarouselImages(),
+    const CustomList(),
+    const Exercice7()
   ];
 
   @override
@@ -24,11 +27,10 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Flutter'),
       ),
-      drawer: const CustomDrawer(),
       body: list[index],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: index,
-        showSelectedLabels: false,
+        showSelectedLabels: true,
         showUnselectedLabels: false,
         onTap: (int i) {
           setState(() {
@@ -39,9 +41,9 @@ class _MyHomePageState extends State<MyHomePage> {
         selectedItemColor: Colors.white,
         iconSize: 48,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Exo 5'),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Exo 6'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Exo 7'),
         ],
       ),
     );
